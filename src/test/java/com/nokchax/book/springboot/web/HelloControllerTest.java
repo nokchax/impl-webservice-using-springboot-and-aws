b3429fd4d95988b6665752bee1a76f7182ae891c
nokchax <nokchax@gmail.com>
2020-08-16 22:38:24 +0900

    study springboot test

--- src/test/java/com/nokchax/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/nokchax/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,12 @@
 package com.nokchax.book.springboot.web;
 
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +22,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * ref) https://engkimbs.tistory.com/768
  */
 //todo @RunWith 와 SpringRunner 에 대하여
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class) // junit4를 사용할 경우 써줘야 함 Junit5부터는 쓸필요 없다
+//@ExtendWith(SpringExtension.class)
 @WebMvcTest // @Controller, @ControllerAdvice 를 사용할 수 있음 / @Service, @Repository, @Component 는 사용 못한다.
 // 그러면 @ControllerAdvice 는..?
 public class HelloControllerTest {
